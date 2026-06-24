e24906497ef35f6717a7f64f908d6bc88dcd50b7
saatwiksingh <saatwiksingh2404@gmail.com>
2026-06-24 22:11:09 +0530

    added models with associations

---
+++ models/models.go
@@ -0,0 +1,37 @@
+package models
+
+
+import(
+	"gorm.io/gorm"
+)
+
+type Movie struct{
+	gorm.Model
+	Title string `json:"title" gorm:"not null"`
+	Rating int	`json:"rating" gorm:"not null;check:rating>=0 AND rating<=10"`
+	Release_year int `json:"release_year" gorm:"not null"`
+	Description string	`json:"description" gorm:"not null"`
+	Reviews []Review `json:"review"`
+	Genres []Genre `json:"genres" gorm:"many2many:movie_genres"`
+}
+
+type User struct{
+	gorm.Model
+	Name string	`json:"name" gorm:"not null"`
+	Email string `json:"email" gorm:"not null"`
+	Reviews []Review  `json:"review"`
+}
+
+type Genre struct{
+	gorm.Model 
+	Name string `json:"name" gorm:"not null;unique"`
+	Movies []Movie `json:"movies" gorm:"many2many:movie_genres"`
+}
+
+type Review struct{
+	gorm.Model
+	UserID int 	`json:"user_id" gorm:"not null"`
+	MovieID int `json:"movie_id" gorm:"not null"`
+	Rating int `json:"rating" gorm:"not null"`
+	Comment string `json:"comment"`
+}
