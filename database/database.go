befd2292ccfa7a8dee3e781aa109abb237eb7380
saatwiksingh <saatwiksingh2404@gmail.com>
2026-06-24 22:23:26 +0530

    database added

---
+++ database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"cinetrack/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+func connect(){
+	dsn:="host=localhost password=saatwik2404 dbanme=moviedb user=postgres sslmode=disable port=5432"
+
+	db,err:=gorm.Open(postgres.Open(dsn),&gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if(err!=nil){
+		panic(err);
+	}
+
+	db.AutoMigrate(&models.Genre{},&models.Movie{},&models.Review{},&models.User{});
+
+	DB=db;
+}
