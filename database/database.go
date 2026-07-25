8c972b1ce541c312f440dc41be93a34e40ed3045
saatwiksingh <saatwiksingh2404@gmail.com>
2026-07-25 08:12:39 +0530

    added .env files

--- database/database.go
+++ database/database.go
@@ -2,7 +2,11 @@ package database
 
 import (
 	"cinetrack/models"
+	"fmt"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -10,7 +14,8 @@ import (
 
 var DB *gorm.DB
 func Connect(){
-	dsn:="host=localhost password=saatwik2404 dbname=moviedb user=postgres sslmode=disable port=5432"
+	godotenv.Load()
+	dsn:=fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",os.Getenv("DB_HOST"),os.Getenv("DB_USER"),os.Getenv("DB_PASSWORD"),os.Getenv("DB_NAME"),os.Getenv("DB_PORT"))
 
 	db,err:=gorm.Open(postgres.Open(dsn),&gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
