32c49edd0537ae98159beb324b8d27b2388c119b
saatwiksingh <saatwiksingh2404@gmail.com>
2026-07-25 08:26:08 +0530

    graceful shutdown added

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-    "cinetrack/database"
-    "cinetrack/routes"
-    "github.com/gin-gonic/gin"
+	"cinetrack/database"
+	"cinetrack/routes"
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
     database.Connect()
     router := gin.Default()
     routes.SetupRoutes(router)
+
+    srv:=&http.Server{
+        Addr:"9092",
+        Handler:router,
+    }
+    go func(){
+         err:=srv.ListenAndServe()
+         if (err!=nil && err!=http.ErrServerClosed){
+            panic(err)
+         }
+    }()
+    quit:=make(chan os.Signal,1)
+    signal.Notify(quit,syscall.SIGINT,syscall.SIGTERM)
+    <-quit
+
+    ctx,canel:=context.WithTimeout(context.Background(),5*time.Second)
+    defer canel()
+    srv.Shutdown(ctx)
     router.Run(":9092")
 }
